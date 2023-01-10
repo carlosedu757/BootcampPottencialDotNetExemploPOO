4f6351295e2d8ee531af6dae438df0134d989c15
Carlos Eduardo <74271104+carlosedu757@users.noreply.github.com>
2023-01-10 20:13:08 -0300

    Add project files.

---
+++ Models/ContaCorrente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootcampPottencialDotNetExemploPOO.Models
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente(int numeroConta, decimal saldoInicial)
+        {
+            NumeroConta = numeroConta;
+            saldo = saldoInicial;
+        }
+        public int NumeroConta { get; set; }
+        private decimal saldo;
+
+        public void Sacar(decimal valor)
+        {
+            saldo -= valor;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using BootcampPottencialDotNetExemploPOO.Models;
+
+Pessoa p1= new Pessoa();
+p1.Nome = "Carlos";
+p1.Idade = 21;
+p1.Apresentar();
